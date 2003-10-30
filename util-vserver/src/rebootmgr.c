90462bf4b2e6f448938f18121a1c2841a186ac19
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2003-10-30 00:36:03 +0000

    use customizable paths for '/usr/sbin'
    git-svn-id: http://svn.linux-vserver.org/svn/util-vserver/branches/SYSCALL_SWITCH@304 94cd875c-1c1d-0410-91d2-eb244daf1a30

--- util-vserver/src/rebootmgr.c
+++ util-vserver/src/rebootmgr.c
@@ -104,13 +104,13 @@ static int rebootmgr_process (int fd, const char *vname)
 		if (strcmp(buf,"reboot\n")==0){
 			char cmd[1000];
 			syslog (LOG_NOTICE,"reboot vserver %s\n",vname);
-			snprintf (cmd,sizeof(cmd)-1,"/usr/sbin/vserver %s restart >>/var/log/boot.log 2>&1",vname);
+			snprintf (cmd,sizeof(cmd)-1,"%s/vserver %s restart >>/var/log/boot.log 2>&1",SBINDIR, vname);
 			system (cmd);
 			ret = 0;
 		}else if (strcmp(buf,"halt\n")==0){
 			char cmd[1000];
 			syslog (LOG_NOTICE,"halt vserver %s\n",vname);
-			snprintf (cmd,sizeof(cmd)-1,"/usr/sbin/vserver %s stop >>/var/log/boot.log 2>&1",vname);
+			snprintf (cmd,sizeof(cmd)-1,"%s/vserver %s stop >>/var/log/boot.log 2>&1",SBINDIR, vname);
 			system (cmd);
 			ret = 0;
 		}else{
