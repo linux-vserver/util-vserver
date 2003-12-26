3f452c6c8e9a3c53ca4c70c1f2faf66d21095f4a
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2003-12-26 00:50:37 +0000

    initial checkin
    git-svn-id: http://svn.linux-vserver.org/svn/util-vserver/trunk@498 94cd875c-1c1d-0410-91d2-eb244daf1a30

---
+++ util-vserver/compat-c99.h
@@ -0,0 +1,31 @@
+// $Id$    --*- c -*--
+
+// Copyright (C) 2003 Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+#ifndef H_UTIL_VSERVER_COMPAT_C99_H
+#define H_UTIL_VSERVER_COMPAT_C99_H
+
+#if defined(__GNUC__) && __GNUC__ < 3 || (__GNUC__==3 && __GNUC_MINOR__<3)
+#  warning Enabling hacks to make it compilable with non-C99 compilers
+#  define BS { do {} while (0)
+#  define BE } do {} while (0)
+#else
+#  define BS do {} while (0)
+#  define BE do {} while (0)
+#endif
+
+#endif	//  H_UTIL_VSERVER_COMPAT_C99_H
