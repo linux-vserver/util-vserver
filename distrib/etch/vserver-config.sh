cb89e96d574857bfdd1bb79c22889ef706d0be81
Benedikt Boehm <hollow@gentoo.org>
2007-04-13 09:02:29 +0000

    add build scripts for debian etch
    git-svn-id: http://svn.linux-vserver.org/svn/util-vserver/trunk@2528 94cd875c-1c1d-0410-91d2-eb244daf1a30

---
+++ distrib/etch/vserver-config.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+REMOVE_LINKS="
+bootlogd
+checkfs
+checkroot
+halt
+hwclock.sh
+ifupdown
+klogd
+libdevmapper1.02
+makedev
+module-init-tools
+mountall.sh
+mountdevsubfs.sh
+mountnfs.sh
+mountkernfs.sh
+mountvirtfs
+networking
+reboot
+setserial
+single
+stop-bootlogd
+stop-bootlogd-single
+umountfs
+umountnfs.sh
+umountroot
+urandom
+"
+
+test -x /usr/sbin/tzconfig && /usr/sbin/tzconfig
+
+aptitude update
+LANG=C aptitude install locales
+
+test -x /usr/sbin/locale-gen && /usr/sbin/locale-gen
+
+dpkg-reconfigure passwd
+tasksel
+
+for link in $REMOVE_LINKS; do
+	update-rc.d -f $link remove
+done
+
