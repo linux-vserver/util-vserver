9a7c44bea1f37fd25f35b391433d65458ed96b13
Daniel Hokka Zakrisson <daniel@hozac.com>
2007-08-16 02:00:23 +0000

    Add fallback syscall numbers, if it's not defined in the headers.
    git-svn-id: http://svn.linux-vserver.org/svn/util-vserver/trunk@2587 94cd875c-1c1d-0410-91d2-eb244daf1a30

--- lib_internal/sys_unshare.h
+++ lib_internal/sys_unshare.h
@@ -21,6 +21,55 @@
 
 #include <unistd.h>
 #include "lib/syscall-wrap.h"
+
+#ifndef __NR_unshare
+#  if defined(__alpha__)
+#    define __NR_unshare	465
+#  elif defined(__arm__)
+#    define __NR_unshare	337
+#  elif defined(__avr32__)
+#    define __NR_unshare	258
+#  elif defined(__blackfin__)
+#    define __NR_unshare	310
+#  elif defined(__frv__)
+#    define __NR_unshare	310
+#  elif defined(__h8300__)
+#    define __NR_unshare	310
+#  elif defined(__i386__)
+#    define __NR_unshare	310
+#  elif defined(__ia64__)
+#    define __NR_unshare	1296
+#  elif defined(__m68knommu__)
+#    define __NR_unshare	303
+#  elif defined(__m68k__)
+#    define __NR_unshare	303
+#  elif defined(__mips__) && (_MIPS_SIM == _MIPS_SIM_ABI32)
+#    define __NR_unshare	303
+#  elif defined(__mips__) && (_MIPS_SIM == _MIPS_SIM_ABI64)
+#    define __NR_unshare	262
+#  elif defined(__mips__) && (_MIPS_SIM == _MIPS_SIM_NABI32)
+#    define __NR_unshare	266
+#  elif defined(__parisc__)
+#    define __NR_unshare	288
+#  elif defined(__powerpc__)
+#    define __NR_unshare	282
+#  elif defined(__s390__)
+#    define __NR_unshare	303
+#  elif defined(__sh64__)
+#    define __NR_unshare	338
+#  elif defined(__sh__)
+#    define __NR_unshare	310
+#  elif defined(__sparc64__)
+#    define __NR_unshare	299
+#  elif defined(__sparc__)
+#    define __NR_unshare	299
+#  elif defined(__x86_64__)
+#    define __NR_unshare	272
+#  else
+#    error Sorry, don't know unshare's syscall number for this architecture.
+#  endif
+#endif
+
 #define __NR_sys_unshare	__NR_unshare
 
 #ifndef ENSC_SYSCALL_TRADITIONAL
