e187a9fcea8b058175231782da65f09275221a31
Daniel Hokka Zakrisson <daniel@hozac.com>
2008-07-15 22:16:01 +0000

    This is not an option used by the utils, but the semantics are for filesystem namespaces.
    git-svn-id: http://svn.linux-vserver.org/svn/util-vserver/trunk@2748 94cd875c-1c1d-0410-91d2-eb244daf1a30

--- src/vcontext.c
+++ src/vcontext.c
@@ -274,8 +274,8 @@ doit(struct Arguments const *args, int argc, char *argv[])
     if (args->do_chroot) {
       Echroot(".");
       if (args->set_namespace) {
-	if (args->do_migrateself)  Evc_set_namespace(xid, 0);
-	else if (args->do_migrate) Evc_enter_namespace(xid, 0);
+	if (args->do_migrateself)  Evc_set_namespace(xid, CLONE_NEWNS|CLONE_FS);
+	else if (args->do_migrate) Evc_enter_namespace(xid, CLONE_NEWNS|CLONE_FS);
       }
     }
 
